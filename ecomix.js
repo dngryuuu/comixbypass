21096e42aa27c96967c2688128493247ebc02918
ryuu <79709879+dngryuuu@users.noreply.github.com>
2026-08-06 05:20:57 +0700

    Update ecomix.js

--- ecomix.js
+++ ecomix.js
@@ -5,10 +5,10 @@ if (body) {
         let obj = JSON.parse(body);
 
         if (obj.data && typeof obj.data === "object") {
-            obj.data.subscriptionType = "premium";
-            obj.data.activeEntitlementId = "weekly";
+            obj.data.subscriptionType = "pro";
+            obj.data.activeEntitlementId = "EasyComix Pro";
             obj.data.subscriptionEndDate = "2099-12-31T23:59:59.000Z";
-            obj.data.subscriptionProvider = "apple";
+            obj.data.subscriptionProvider = "APP_STORE";
             obj.success = true;
             body = JSON.stringify(obj);
             console.log("================ [EASYCOMIX - AFTER] ================");
