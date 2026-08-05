ac7adad898ad2a89ab7a5959ef38382a957ab14a
ryuu <79709879+dngryuuu@users.noreply.github.com>
2026-08-06 06:40:15 +0700

    Create quota.js

---
+++ quota.js
@@ -0,0 +1,22 @@
+let body = $response.body;
+
+if (body) {
+    try {
+        let obj = JSON.parse(body);
+
+        if (obj.data && typeof obj.data === "object") {
+            obj.data.quota.tier = "pro";
+            obj.data.quota.remaining = "9999";
+            obj.data.liveQuota.tier = "pro";
+            obj.data.liveQuota.remaining = "9999";
+            obj.success = true;
+            body = JSON.stringify(obj);
+            console.log("================ [EASYCOMIX QUOTA - AFTER] ================");
+            console.log(body);
+        }
+    } catch (e) {
+        console.log("Error: " + e);
+    }
+}
+
+$done({ body });
