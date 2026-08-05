92c4aed1da89fd9ee96f2fae20c00913f3b3cbab
ryuu <79709879+dngryuuu@users.noreply.github.com>
2026-08-06 04:58:13 +0700

    Update ecomix.js

--- ecomix.js
+++ ecomix.js
@@ -6,7 +6,7 @@ if (body) {
 
         if (obj.data && typeof obj.data === "object") {
             obj.data.subscriptionType = "pro";
-            obj.data.activeEntitlementId = "unlimited";
+            obj.data.activeEntitlementId = "weekly";
             obj.data.subscriptionEndDate = "2099-12-31T23:59:59.000Z";
             obj.data.subscriptionProvider = "apple";
             obj.success = true;
